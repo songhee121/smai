0e532fcc5a270c65bd62be70d61fb2f0c03ba0e6
songhee <hehe9331@gmail.com>
2025-08-18 15:24:30 +0900

    250818

--- day01/p1.py
+++ day01/p1.py
@@ -1,1 +1,12 @@
-print('ok')
+a = 10
+b = 10.1
+c = 'a'
+d = 'aaa'
+e = True
+
+if __name__ == '__main__':
+    print('%d = %.2f , %c '%(a,b,c))
+    print("결과 값 {0} {1} ".format(a, d))
+    print("결과 값 {aa} {bb} ".format(aa=a, bb=d))
+    print(f"결과 값 {a} {b} ")
+    print(f"결\t과\t\'값\'\t\r{a}\n{b} ")
---
+++ day01/p2.py
@@ -0,0 +1,12 @@
+a = 10
+b = 10.1567
+c = 'a'
+d = 'aaa'
+e = True
+
+if __name__ == '__main__':
+    print(b)
+    print(type(b))
+    print(type(a))
+    print('결과값:%d이고 %.3f'%(a, b))
+    print(f'결과는 {a} {b} {e}입니다.')
