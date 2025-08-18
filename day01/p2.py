13499efab5e7fb0ef12c8bd2afa81d1bde8524fa
songhee <hehe9331@gmail.com>
2025-08-18 15:35:43 +0900

    250818

--- day01/p2.py
+++ day01/p2.py
@@ -1,12 +1,20 @@
 a = 10
 b = 10.1567
 c = 'a'
-d = 'aaa'
+d = '"aaa"'
 e = True
+num1="100"
+num2="200"
+result= int(num1)+int(num2)
+
 
 if __name__ == '__main__':
+    #print(num1,'+', num2, '=', result)
+    print(f"결과는 {d}")
     print(b)
     print(type(b))
     print(type(a))
+    """
     print('결과값:%d이고 %.3f'%(a, b))
     print(f'결과는 {a} {b} {e}입니다.')
+    """
