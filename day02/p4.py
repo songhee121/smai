f88f7f31ee80bf5fd50c3c1afe5bd7f45b376a40
songhee121 <hehe9331@gmail.com>
2025-08-19 11:39:52 +0900

    250819

--- day02/p4.py
+++ day02/p4.py
@@ -30,12 +30,27 @@ if __name__ == '__main__':
     print(d2["age"])
 
     sts=[
-        {"name": "정태원", "age": 32, "major": "ee"}
-        {"name": "김송희", "age": 23, "major": "ee"}
-        {"name": "조태희", "age": 28, "major": "sw"}
+        {"name": "정태원", "age": 32, "major": "ee"},
+        {"name": "김송희", "age": 23, "major": "ee"},
+        {"name": "조태희", "age": 28, "major": "sw"},
         {"name": "머하지", "age": 20, "major": "sw"}
     ]
+    print("\n")
+
+    #응용
+    #전체 학과 학생의 나이 합과 평균
+    sum=0
+    for i in sts:
+        sum+=i["age"]
+
+    print(f'전체 학과 학생 나이 합은 {sum}, 평균은 {sum/len(sts)}입니다')
 
     #ee 학과 학생의 나이 합과 평균
-    for i in list1:
-        sum+=i
+    sum=0
+    len=0
+    for i in sts:
+        if i["major"]=="ee":
+            sum+=i["age"]
+            len+=1
+
+    print(f'전자과 학생 나이 합은 {sum}, 평균은 {sum/len}입니다')
