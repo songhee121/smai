d0d183d68ca894fcbad72105e36857464b4c16da
songhee121 <hehe9331@gmail.com>
2025-08-19 13:23:37 +0900

    250819

---
+++ day02/p5.py
@@ -0,0 +1,17 @@
+if __name__ == '__main__':
+    #File Write & Read
+    with open("data/mytext.txt", "w", encoding="utf-8")as f:
+        print("파일 라인1", file=f)
+        f.write("파일 Write")
+        f.close()
+
+    with open("data/mytext.txt", "r", encoding="utf-8")as f:
+        data=f.readline()
+        print(data)
+        f.close()
+
+    with open("data/mytext.txt", "r", encoding="utf-8")as f:
+        data=f.readlines()
+        for d in data:
+            print(d)
+        f.close()
