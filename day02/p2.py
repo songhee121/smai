ec3a429f3393514d6d4a1dc042ea4fd4c54ddef9
songhee121 <hehe9331@gmail.com>
2025-08-19 11:04:06 +0900

    250819

---
+++ day02/p2.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    list1=[1,2,3,4,5]
+    print(type(list1))
+    print(list1)
+    list1.append(5)
+    print(list1)
+    list1.insert(1, 99)
+    print(list1)
+    list1.remove(3)
+    print(list1)
---
+++ day02/p3.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    #list
+    list1 =[1,2,3,4,5]
+    sum=0
+    for i in list1:
+        sum+=i
+    print(f'합은 {sum}')
